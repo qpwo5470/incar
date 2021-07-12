fb3cedd831bde63a75dacbe2958ac7dcc8f56468
qpwo5470 <qpwo5470@gmail.com>
2021-07-13 01:52:39 +0900

    BrokenPipeError added

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def socketthread():
                 data['accel'] = acc
                 sock.sendall(bytes(json.dumps(data), encoding="utf-8"))
                 time.sleep(1/15)
-        except [ConnectionRefusedError, ConnectionAbortedError, ConnectionResetError]:
+        except [ConnectionRefusedError, ConnectionAbortedError, ConnectionResetError, BrokenPipeError]:
             pass
 
 
