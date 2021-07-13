f7906ccd673174f670e7972b05d39af633d09d43
qpwo5470 <qpwo5470@gmail.com>
2021-07-13 18:05:20 +0900

    gear added

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import time
 import threading
 import socket
 import json
-import random
 
 
-line = [] #라인 단위로 데이터 가져올 리스트 변수
+line = []
 acc = 0
+p_button = 0
+gear = 'N'
 data = {}
 
-port = '/dev/ttyUSB0' # 시리얼 포트
-baud = 9600 # 시리얼 보드레이트(통신속도)
+portA = '/dev/ttyUSB0'
+portB = '/dev/ttyUSB1'
+baud = 9600
 
 
 def socketthread():
     global acc
+    global p_button
+    global gear
     global data
 
     while True:
@@ -24,6 +28,8 @@ def socketthread():
             sock.connect(("192.168.1.69", 9999))
             while True:
                 data['accel'] = acc
+                data['P'] = p_button
+                data['gear'] = gear
                 sock.sendall(bytes(json.dumps(data), encoding="utf-8"))
                 time.sleep(1/15)
         except [ConnectionRefusedError, ConnectionAbortedError, ConnectionResetError, BrokenPipeError]:
@@ -33,22 +39,34 @@ def socketthread():
 def serialthread(ser):
     global line
     global acc
+    global p_button
+    global gear
 
     while True:
         for c in ser.read():
             if c == 10:
                 text = ''.join(line).strip()
-                if text.isnumeric():
-                    acc = int(text)
-                else:
-                    print(text)
-                del line[:]
+                if text[0] == 'A':
+                    text = text[1:]
+                    if text.isnumeric():
+                        acc = int(text)
+                    else:
+                        print(text)
+                    del line[:]
+                elif text[0] == 'D':
+                    text = text[1:]
+                    temp = text.split('/')
+                    p_button = int(temp[0])
+                    gear = temp[1]
             else:
                 line.append(chr(c))
 
 
 if __name__ == "__main__":
-    ser = serial.Serial(port, baud, timeout=0)
+    ser = serial.Serial(portA, baud, timeout=0)
+    serial_thread = threading.Thread(target=serialthread, args=(ser,))
+    serial_thread.start()
+    ser = serial.Serial(portB, baud, timeout=0)
     serial_thread = threading.Thread(target=serialthread, args=(ser,))
     serial_thread.start()
     socket_thread = threading.Thread(target=socketthread(), args=())
