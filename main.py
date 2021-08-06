30d91241632fb25ce043b3d67c4c112034c9e993
qpwo5470 <qpwo5470@gmail.com>
2021-08-06 23:07:17 +0900

    test

--- main.py
+++ main.py
@@ -94,16 +94,22 @@ def serialthread(ser):
                 line.append(chr(c))
 
 if __name__ == "__main__":
-    for port in serial_ports():
-        ser = serial.Serial(port, baud, timeout=0)
-        serial_thread = threading.Thread(target=serialthread, args=(ser,))
-        serial_thread.daemon = True
-        serial_thread.start()
+    try:
+        for port in serial_ports():
+            ser = serial.Serial(port, baud, timeout=0)
+            serial_thread = threading.Thread(target=serialthread, args=(ser,))
+            serial_thread.daemon = True
+            serial_thread.start()
 
-    receiver_thread = threading.Thread(target=receiverthread(), args=())
-    receiver_thread.daemon = True
-    receiver_thread.start()
+        receiver_thread = threading.Thread(target=receiverthread(), args=())
+        receiver_thread.daemon = True
+        receiver_thread.start()
 
-    sender_thread = threading.Thread(target=senderthread(), args=())
-    sender_thread.daemon = True
-    sender_thread.start()
+        sender_thread = threading.Thread(target=senderthread(), args=())
+        sender_thread.daemon = True
+        sender_thread.start()
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sock.close()
